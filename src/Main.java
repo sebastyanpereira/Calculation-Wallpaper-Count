ed4de31419f10fdabf705e8b74a29e36b23347c4
dmitry.novikov <student@ya.ru>
2019-09-26 15:29:10 +0300

    Added user input + calculation count rolls wallpaper

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter width room in meters: ");
+        double widthRoom = keyboard.nextDouble();
+        System.out.println("Please enter length room in meters: ");
+        double lengthRoom = keyboard.nextDouble();
+        System.out.println("Please enter height room in meters: ");
+        double heightRoom = keyboard.nextDouble();
+        System.out.println("Please enter width one roll of wallpaper in meters: ");
+        double widthWallpaper = keyboard.nextDouble();
+        System.out.println("Please enter length one roll of wallpaper in meters: ");
+        double lengthWallpaper = keyboard.nextDouble();
+        double reserveComfort  = 0.1;
+        double perimeterRoom = (widthRoom + lengthRoom) * 2;
+        int countBandForRoom = (int) Math.ceil(perimeterRoom / widthWallpaper);
+        int countBandInRoll = (int) (lengthWallpaper / (heightRoom + reserveComfort));
+        int countRollIsNeed = countBandForRoom / countBandInRoll;
+        System.out.println("You need to buy " + countRollIsNeed + " rolls wallpaper");
     }
 }
